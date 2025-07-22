f3cf32eb086236521d9f5659f1e397dab96bdd98
Ak <Akshat76845>
2025-07-22 20:38:52 +0530

    Frontend Files

---
+++ Frontend/src/index.ts
@@ -0,0 +1,37 @@
+import {WebSocket,WebSocketServer} from "ws";
+const wss = new WebSocketServer({port:8080});
+
+let allSockets: WebSocket[] = [];
+wss.on("connection",(socket)=>{
+    allSockets.push(socket);
+    console.log("user connected");
+    socket.on("message",(message) => {
+        console.log("message received" + message.toString())
+        const parsedMessage = JSON.parse(message);
+        if(parsedMessage.type === "join"){
+        allSockets.push({
+            socket,
+            room: parsedMessage.payload.roomId
+        })
+      }
+
+      if(parsedMessage.type === "chat"){
+        let currentUserRoom = null;
+        for(let i=0;i< allSockets.length;i++){
+            if(allSockets[i].socket === socket){
+                currentUserRoom = allSockets[i].room
+            }   
+        }
+        for(let i=0;i< allSockets.length;i++){
+            if(allSockets[i].room == currentUserRoom){
+                allSockets[i].socket.send(parsedMessage.payload.message)
+            }
+        }
+      }
+    })
+
+    socket.on("disconnect",()=>{
+        allSockets = allSockets.filter(x=>x!= socket);
+    })
+})
+
