006deea6c8109210408050e88a4ffca6c37c9272
Ak <Akshat76845>
2025-07-22 18:46:22 +0530

    Second commit

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
 import {WebSocket,WebSocketServer} from "ws";
 const wss = new WebSocketServer({port:8080});
-interface User{
-    user: string,
-    name: string
-};
+
 let allSockets: WebSocket[] = [];
 wss.on("connection",(socket)=>{
     allSockets.push(socket);
     console.log("user connected");
     socket.on("message",(message) => {
         console.log("message received" + message.toString())
-        allSockets.forEach(s=>{
-            s.send(message.toString() + ": sent from the server");
+        const parsedMessage = JSON.parse(message);
+        if(parsedMessage.type === "join"){
+        allSockets.push({
+            socket,
+            room: parsedMessage.payload.roomId
         })
+      }
+
+      if(parsedMessage.type === "chat"){
+        let currentUserRoom = null;
+        for(let i=0;i< allSockets.length;i++){
+            if(allSockets[i].socket === socket){
+                currentUserRoom = allSockets[i].room
+            }   
+        }
+        for(let i=0;i< allSockets.length;i++){
+            if(allSockets[i].room == currentUserRoom){
+                allSockets[i].socket.send(parsedMessage.payload.message)
+            }
+        }
+      }
     })
 
     socket.on("disconnect",()=>{
         allSockets = allSockets.filter(x=>x!= socket);
     })
 })
+
